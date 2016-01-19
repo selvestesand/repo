6f0945c53b2a878c753733ef77d4db760274fab9
selvestesand <selvestesand@gmail.com>
2016-01-19 22:31:00 +0100

    Revert "Master"

--- PartyPlanner/PartyPlanner/BirthdayParty.cs
+++ PartyPlanner/PartyPlanner/BirthdayParty.cs
@@ -6,26 +6,67 @@ using System.Threading.Tasks;
 
 namespace PartyPlanner
 {
-    class BirthdayParty : Party
+    class BirthdayParty
     {
 
-        //private int numberOfPeople;
-        //public int NumberOfPeople
-        //{
-        //    set { numberOfPeople = value; }
-        //}
+        public const int CostOfFoodPerPerson = 25;
 
-        //private bool fancyDecorations;
-        //public bool FancyDecorations
-        //{
-        //    set { fancyDecorations = value; }
-        //}
+        private const decimal FancyDecoPerPerson = 15.00M;
+        private const decimal FancyDecoFee = 50.00M;
+        private const decimal NonFancyDecoPerPerson = 7.50M;
+        private const decimal NonFancyDecoFee = 30.00M;
 
-        //// Returns total cost of decorations
-        //private decimal CalculateCostOfDecorations()
-        //{
-        //    return fancyDecorations ? (numberOfPeople * FancyDecoPerPerson) + FancyDecoFee : (numberOfPeople * NonFancyDecoPerPerson) + NonFancyDecoFee;
-        //}
+        private int numberOfPeople;
+        public int NumberOfPeople
+        {
+            set { numberOfPeople = value; }
+        }
+
+        private string cakeWriting;
+        public string CakeWriting
+        {
+            set { cakeWriting = value; }
+        }
+
+        private int ActualLength
+        {
+            get { return cakeWriting.Length > MaxWritingLength() ? MaxWritingLength() : cakeWriting.Length; }
+        }
+
+        public bool CakeWritingTooLong
+        {
+            get { return cakeWriting.Length > MaxWritingLength() ? true : false; }
+        }
+
+        private bool fancyDecorations;
+        public bool FancyDecorations
+        {
+            set { fancyDecorations = value; }
+        }
+
+        // Returns size of the cake.
+        private int CakeSize()
+        {
+            return numberOfPeople <= 4 ? 8 : 16;
+        }
+
+        // Returns the max cake writing length
+        private int MaxWritingLength()
+        {
+            return CakeSize() == 8 ? 16 : 40;
+        }
+
+        // Returns total cost of the party
+        public decimal Cost
+        {
+            get
+            {
+                decimal totalCost = CalculateCostOfDecorations();
+                totalCost += CostOfFoodPerPerson * numberOfPeople;
+                decimal cakeCost = CakeSize() == 8 ? 40M + (ActualLength * .25M) : 75M + (ActualLength * .25M);
+                return totalCost + cakeCost;
+            }
+        }
 
         /// <summary>
         /// BirthdayParty constructor
@@ -40,50 +81,11 @@ namespace PartyPlanner
             CakeWriting = cakeWriting;
         }
 
-        private string cakeWriting;
-        public string CakeWriting
+        // Returns total cost of decorations
+        private decimal CalculateCostOfDecorations()
         {
-            set { cakeWriting = value; }
+            return fancyDecorations ? (numberOfPeople * FancyDecoPerPerson) + FancyDecoFee : (numberOfPeople * NonFancyDecoPerPerson) + NonFancyDecoFee;
         }
-
-        public bool CakeWritingTooLong
-        {
-            get { return cakeWriting.Length > MaxWritingLength() ? true : false; }
-        }
-
-        private int ActualLength
-        {
-            get { return cakeWriting.Length > MaxWritingLength() ? MaxWritingLength() : cakeWriting.Length; }
-        }
-
-        // Returns total cost of the party
-        public override decimal Cost
-        {
-            get
-            {
-                //Adding base cost
-                decimal totalCost = base.Cost;
-                //Adding cost of the cake
-                totalCost += CakeSize() == 8 ? 40M + (ActualLength * .25M) : 75M + (ActualLength * .25M);
-
-                return totalCost;
-            }
-        }
-
-        // Returns size of the cake.
-        private int CakeSize()
-        {
-            return NumberOfPeople <= 4 ? 8 : 16;
-        }
-
-        // Returns the max cake writing length
-        private int MaxWritingLength()
-        {
-            return CakeSize() == 8 ? 16 : 40;
-        }
-
-
-
 
     }
 }
--- PartyPlanner/PartyPlanner/DinnerParty.cs
+++ PartyPlanner/PartyPlanner/DinnerParty.cs
@@ -6,80 +6,84 @@ using System.Threading.Tasks;
 
 namespace PartyPlanner
 {
-    class DinnerParty : Party
+    class DinnerParty
     {
-
-        //Set Fancy Deconrations option
-        //private bool fancyDecorations;
-        //public bool FancyDecorations {
-
-        //Set number of people attending the dinner party
-        //private int numberOfPeople;
-        //public int NumberOfPeople {
-        //    set
-        //    {
-        //        numberOfPeople = value;
-        //        CalculateCostOfDecorations(); !!ADD THIS TO Cost PROPERTY!!
-        //    }
-        //}
-
-        //    private get
-        //    {
-        //        return fancyDecorations;
-        //    }
-        //    set
-        //    {
-        //        fancyDecorations = value;
-        //        CalculateCostOfDecorations();
-        //    }
-        //}
-
-        //Calculates the cost of decorations
-        //private decimal CalculateCostOfDecorations()
-        //{
-        //    return fancyDecorations ? (numberOfPeople * FancyDecoPerPerson) + FancyDecoFee : (numberOfPeople * NonFancyDecoPerPerson) + NonFancyDecoFee;
-        //}
-
+        private const int CostOfFoodPerPerson = 25;
         private const decimal HealthyBeverage = 5.00M;
         private const decimal HealthyDiscount = .95M;
         private const decimal NonHealthyBeverage = 20.00M;
+        private const decimal FancyDecoPerPerson = 15.00M;
+        private const decimal FancyDecoFee = 50.00M;
+        private const decimal NonFancyDecoPerPerson = 7.50M;
+        private const decimal NonFancyDecoFee = 30.00M;
 
-        // DinnerParty constructor 
-        public DinnerParty(int numberOfPeople, bool healthyOption, bool fancyDecorations)
-        {
-            HealthyOption = healthyOption;
-            FancyDecorations = fancyDecorations;
-            NumberOfPeople = numberOfPeople;
+        private decimal CostOfBeveragesPerPerson;
+        private decimal CostOfDecorations;
+
+
+        //Set number of people attending the dinner party
+        private int numberOfPeople;
+        public int NumberOfPeople {
+            set
+            {
+                numberOfPeople = value;
+                CalculateCostOfDecorations();
+            }
         }
 
         //Set Healthy beverage option
         private bool healthyOption;
-        public bool HealthyOption
-        {
-            set { healthyOption = value; }
+        public bool HealthyOption {
+            set
+            {
+                healthyOption = value;
+                CostOfBeveragesPerPerson = healthyOption ? HealthyBeverage : NonHealthyBeverage;
+            }
         }
 
-        //Returns the cost of the entire dinner party.
-        public override decimal Cost
-        {
-            get
-            {
-                //Adding base cost
-                decimal totalCost = base.Cost;
-                //Adding cost of beverages
-                totalCost += healthyOption ? HealthyBeverage * NumberOfPeople : NonHealthyBeverage * NumberOfPeople;
-                //Adding potential discount
-                totalCost = healthyOption ? totalCost * HealthyDiscount : totalCost;
+        //Set Fancy Deconrations option
+        private bool fancyDecorations;
+        public bool FancyDecorations {
 
-                return totalCost;
+            private get
+            {
+                return fancyDecorations;
+            }
+            set
+            {
+                fancyDecorations = value;
+                CalculateCostOfDecorations();
             }
         }
 
 
-        private decimal CalculateCostOfBeveragesPerPerson()
+        //Returns the cost of the entire dinner party.
+        public decimal Cost
         {
-            throw new NotImplementedException();
+            get
+            {
+                decimal cost = (((CostOfBeveragesPerPerson + CostOfFoodPerPerson) * numberOfPeople) + CostOfDecorations);
+                cost += numberOfPeople > 12 ? 100M : 0M;
+                return healthyOption ? cost * HealthyDiscount : cost; ;
+            }
         }
+
+
+        // DinnerParty constructor 
+        public DinnerParty(int numberOfPeople, bool healtyOption, bool fancyDecorations)
+        {
+            HealthyOption = healtyOption;
+            FancyDecorations = fancyDecorations;
+            NumberOfPeople = numberOfPeople;
+        }
+
+
+        //Calculates the cost of decorations
+        private void CalculateCostOfDecorations()
+        {
+            CostOfDecorations = fancyDecorations ? (numberOfPeople * FancyDecoPerPerson) + FancyDecoFee : (numberOfPeople * NonFancyDecoPerPerson) + NonFancyDecoFee;
+        }
+
 
     }
 }
